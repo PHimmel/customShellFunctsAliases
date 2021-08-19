afbc56fc3d5680c6af0655fd8376a6d7fe1f7c32
Phimmel <peterhimmelman@yahoo.com>
2021-08-19 19:19:32 -0400

    modified:   install.sh 	renamed:    src/updateAndSecurity.zsh -> src/updateAndSecurity.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-echo "${PWD}, ${0}, ${SHELL}"
+# echo "${PWD}, ${0}, ${SHELL}"
+
+echo "Install started. . .\n"
 
 if [[ "${SHELL}" = *"zsh"* ]] ; then
-    echo "zsh"
+    echo "Shell is zsh" ; echo "source ${PWD}/src/updateAndSecurity.sh" >> ${HOME}/.zshrc
 elif [[ "${SHELL}" = *"bash"* ]] ; then
-    echo "bash"
-else echo "Shell not supported."
+    echo "Shell is bash" ; echo "source ${PWD}/src/updateAndSecurity.sh" >> ${HOME}/.bashrc
+else echo "Shell not supported." ; return
 fi
+
+echo "Install finished!\n"
